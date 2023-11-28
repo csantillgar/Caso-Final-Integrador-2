cd0fbf02ef86978bd76b88bdf08ccf01171c2293
csantillgar <csantill@myuax.com>
2023-11-28 12:05:42 +0100

    1

---
+++ Caso final integrador 2/json11.cpp
@@ -0,0 +1,5 @@
+//
+// Created by cinti on 28/11/2023.
+//
+
+#include "json11.hpp"
---
+++ Caso final integrador 2/main.cpp
@@ -0,0 +1,6 @@
+#include <vector>
+#include <string>
+#include <map>
+#include "json11.hpp"
+#include "jsonlib.hpp"
+enum variant_type { Symbol, Number, List, Proc, Lambda, Cadena };
